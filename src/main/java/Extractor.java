1ebe1d48d0d2ecfa2cc1a4391226021dfa3a5c03
unknown <endzikri@gmail.com>
2020-11-22 17:22:56 +0100

    small changes

--- src/main/java/Extractor.java
+++ src/main/java/Extractor.java
@@ -1,4 +1,3 @@
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -8,7 +7,6 @@ import org.jsoup.select.Elements;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 
@@ -36,7 +34,6 @@ public class Extractor {
                     String link = page.attr("href");
                     String host = "https://www.homegate.ch/rent";
                     if (link.contains(host)) {
-                        System.out.println("PROSLOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
                         if (links.add(URL)) { //dodaj URL
                             System.out.println("dodato:" + URL);
                         }
@@ -61,16 +58,9 @@ public class Extractor {
                 for (Element article : articleLinks) {
                     System.out.println("article");
                     System.out.println(article);
-
-//                    if (article.text().matches("*")) {
-                    System.out.println("CLAN"+  article.attr("href")+" KRAJ CLANA");
-                    //TODO ne mogu da izdvojim  clanove sa href oznakom
-
                     ArrayList<String> temporary = new ArrayList<String>();
                     temporary.add(article.text()); //naclov clana
-                    temporary.add(article.attr("href")); //URL clana koji TODO ne mogu da izdvojim
                     articles.add(temporary);
-//                    }
                 }
             } catch (IOException e) {
                 System.err.println(e.getMessage());
